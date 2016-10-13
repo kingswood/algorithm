3d2c8aa8172bba89fa58db3b271f88f183aba178
kingswood <kingswood@kingswood-PC>
2016-10-13 23:21:46 +0800

    update

--- src/graph/ShortestPathDijkstra.java
+++ src/graph/ShortestPathDijkstra.java
@@ -1,31 +1,80 @@
 package graph;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ShortestPathDijkstra {
-	
-	public static void initialGraph(){
-		
-		Vertex<String> a = new Vertex<String>("A");
-		Vertex<String> c = new Vertex<String>("C");
-		Vertex<String> d = new Vertex<String>("D");
-		Vertex<String> f = new Vertex<String>("F");
-		Vertex<String> k = new Vertex<String>("K");
-		Vertex<String> y = new Vertex<String>("Y");
-		Vertex<String> t = new Vertex<String>("T");
-		Vertex<String> z = new Vertex<String>("Z");
-		Vertex<String> q = new Vertex<String>("Q");
-		
-		a.addEdge(c, 2);
-		a.addEdge(f, 8);
-		a.addEdge(d, 4);
-		
-		c.addEdge(y, 1);
-		
-		y.addEdge(f, 3);
-		
-		f.addEdge(d, 5);
-		
-		d.addEdge(k, 6);
-		
+
+	static int max = Integer.MAX_VALUE;
+
+	private static String[] vertexes = { "A", "C", "D", "T", "B", "E", "F" };
+
+	private static int[][] edges = { 
+			{ 0, 1, 5, max, max, max, max }, 
+			{ 1, 0, max, max, 8, max, max },
+			{ 5, max, 0, 2, max, max, max }, 
+			{ max, max, 2, 0, 1, max, 4 }, 
+			{ max, 8, max, 1, 0, 3, max },
+			{ max, max, max, max, 3, 0, 2 }, 
+			{ max, max, max, 4, max, 2, 0 } };
+
+	public static int[] getShortestPath() {
+
+		int[] calculatedShortestPath = new int[vertexes.length];
+
+		int[] unCalculatedShortestPath = new int[vertexes.length];
+
+		for (int i = 0; i < vertexes.length; i++) {
+			unCalculatedShortestPath[i] = max;
+		}
+
+		for (int i = 0; i < vertexes.length; i++) {
+
+			int temp = -1;
+
+			unCalculatedShortestPath[i] = -1;
+			
+			for (int k = 0; k < unCalculatedShortestPath.length; k++) {
+				
+			}
+
+			for (int j = 0; j < edges.length; j++) {
+				if (edges[i][j] < max && edges[i][j] < temp) {
+					temp = edges[i][j];
+				}
+			}
+
+			calculatedShortestPath[i] = temp;
+
+			unCalculatedShortestPath[i] = -1;
+
+			for (int k = 0; k < unCalculatedShortestPath.length; k++) {
+				if (unCalculatedShortestPath[k] == -1) {
+					continue;
+				} else if (edges[i][k] < max) {
+					if (unCalculatedShortestPath[k] < edges[i][k]) {
+						unCalculatedShortestPath[k] = temp + unCalculatedShortestPath[k];
+					} else {
+						unCalculatedShortestPath[k] = temp + edges[i][k];
+					}
+
+				}
+			}
+
+		}
+
+		return calculatedShortestPath;
+
 	}
-	
+
+	public static void main(String[] args) {
+
+		int[] shortestPath = getShortestPath();
+
+		for (int i = 0; i < shortestPath.length; i++) {
+			System.out.println(shortestPath[i]);
+		}
+
+	}
+
 }
